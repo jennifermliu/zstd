46d35a67e3b0a20a3005102d6232d5ec47d4d810
Jennifer Liu <jenniferliu620@fb.com>
2018-08-01 11:36:26 -0700

    Double number of epochs to make each epoch smaller

--- contrib/experimental_dict_builders/fastCover/fastCover.c
+++ contrib/experimental_dict_builders/fastCover/fastCover.c
@@ -367,7 +367,7 @@ static size_t FASTCOVER_buildDictionary(const FASTCOVER_ctx_t *ctx, U32 *freqs,
   /* Divide the data up into epochs of equal size.
    * We will select at least one segment from each epoch.
    */
-  const U32 epochs = MAX(1, (U32)(dictBufferCapacity / parameters.k));
+  const U32 epochs = MAX(1, (U32)(dictBufferCapacity / parameters.k * 2));
   const U32 epochSize = (U32)(ctx->nbDmers / epochs);
   size_t epoch;
   DISPLAYLEVEL(2, "Breaking content into %u epochs of size %u\n", epochs,
